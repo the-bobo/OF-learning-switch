e916a98a413d45d852dbd19e22dd34886aeffbb0
bobo <bobo@bobos-MacBook.local>
2014-10-05 22:47:45 -0400

    First commit for Assignment 3

---
+++ src/main/java/net/floodlightcontroller/Assignment3/Assignment3.java
@@ -0,0 +1,144 @@
+package net.floodlightcontroller.Assignment3;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.openflow.protocol.OFMessage;
+import org.openflow.protocol.OFType;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.IOFSwitch.PortChangeType;
+import net.floodlightcontroller.core.IOFSwitchListener;
+import net.floodlightcontroller.core.ImmutablePort;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryListener;
+import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryService;
+
+public class Assignment3 implements ILinkDiscoveryListener, IOFMessageListener,
+		IFloodlightModule, IOFSwitchListener {
+	
+	protected ILinkDiscoveryService linkDiscoverer;
+	protected IFloodlightProviderService floodlightProvider;
+
+	@Override
+	public void init(FloodlightModuleContext context)
+			throws FloodlightModuleException {
+		// From Slides
+		this.floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		// add self as one of switch event listeners
+		this.floodlightProvider.addOFSwitchListener(this);
+		
+		this.linkDiscoverer = context.getServiceImpl(ILinkDiscoveryService.class);
+		//add self as one of link event listeners
+		this.linkDiscoverer.addListener(this);
+
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void switchAdded(long switchId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void switchRemoved(long switchId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void switchActivated(long switchId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void switchPortChanged(long switchId, ImmutablePort port,
+			PortChangeType type) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void switchChanged(long switchId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context)
+			throws FloodlightModuleException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public net.floodlightcontroller.core.IListener.Command receive(
+			IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void linkDiscoveryUpdate(LDUpdate update) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void linkDiscoveryUpdate(List<LDUpdate> updateList) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
