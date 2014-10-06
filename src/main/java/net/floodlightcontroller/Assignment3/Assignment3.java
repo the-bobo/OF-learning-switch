6ea2c1a12b53d6d0eaa4c10b958287965c3b5d2a
bobo <bobo@bobos-MacBook.local>
2014-10-06 17:18:04 -0400

    Got the floodlight.properties thing working

--- src/main/java/net/floodlightcontroller/Assignment3/Assignment3.java
+++ src/main/java/net/floodlightcontroller/Assignment3/Assignment3.java
@@ -38,6 +38,7 @@ public class Assignment3 implements ILinkDiscoveryListener, IOFMessageListener,
 	
 	protected ILinkDiscoveryService linkDiscoverer;
 	protected IFloodlightProviderService floodlightProvider;
+	protected Map<Link, LinkInfo> links;
 	protected static Logger log = LoggerFactory.getLogger(Assignment3.class);
 
 	@Override
@@ -59,18 +60,57 @@ public class Assignment3 implements ILinkDiscoveryListener, IOFMessageListener,
 		this.linkDiscoverer.addListener(this);
 		
 		// Get Map of current topology
-		Map<Link, LinkInfo> links = this.linkDiscoverer.getLinks();
-		// won't work log.error(this.linkDiscoverer.getLinks());
-		
+		links = this.linkDiscoverer.getLinks();
+
 		// Run OSPF on the 'links' Map
+		// Probably should do this as a separate method that happens every time
+		// the links object changes
+		
+		/*System.out.println("Inside init()");
+		System.out.println("Inside init()");
+		System.out.println("Inside init()");*/
 		
 
 	}
 	
+	@Override
+	public void linkDiscoveryUpdate(LDUpdate update) {
+		// TODO Auto-generated method stub
+		System.out.println("Inside linkDiscoveryUpdate)");
+		for (Map.Entry<Link, LinkInfo> entry: links.entrySet()) {
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+		/*System.out.println("Inside linkDiscoveryUpdate)");
+		System.out.println("Inside linkDiscoveryUpdate)");
+		System.out.println("Inside linkDiscoveryUpdate)");*/
+
+	}
+
+	@Override
+	public void linkDiscoveryUpdate(List<LDUpdate> updateList) {
+		// TODO Auto-generated method stub
+		System.out.println("Inside linkDiscoveryUpdate LIST)");
+		for (Map.Entry<Link, LinkInfo> entry: links.entrySet()) {
+			System.out.println("Iterating through the Map object lists");
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+		/*System.out.println("Inside linkDiscoveryUpdate(List<LDUpdate> updateList)");
+		System.out.println("Inside linkDiscoveryUpdate(List<LDUpdate> updateList)");
+		System.out.println("Inside linkDiscoveryUpdate(List<LDUpdate> updateList)");*/
+
+	}
+
+	
     public static void main(String[] args) throws FloodlightModuleException {
-        // Setup logger
+    	
+    	// Setup logger
         System.setProperty("org.restlet.engine.loggerFacadeClass", 
                 "org.restlet.ext.slf4j.Slf4jLoggerFacade");
+        
+        // DEBUG STATEMENTS THAT WORK
+        // Both of these work
+        //System.out.println("Enteirng public static void main qqqq");
+        //log.error("Enteirng public static void main lllll");
         
         CmdLineSettings settings = new CmdLineSettings();
         CmdLineParser parser = new CmdLineParser(settings);
@@ -90,6 +130,7 @@ public class Assignment3 implements ILinkDiscoveryListener, IOFMessageListener,
         // Run the main floodlight module
         IFloodlightProviderService controller =
                 moduleContext.getServiceImpl(IFloodlightProviderService.class);
+        
         // This call blocks, it has to be the last line in the main
         controller.run();
     }
@@ -99,67 +140,105 @@ public class Assignment3 implements ILinkDiscoveryListener, IOFMessageListener,
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside getName()");
+		System.out.println("Inside getName()");
+		System.out.println("Inside getName()");*/
+		
 		return null;
 	}
 
+	
 	@Override
 	public boolean isCallbackOrderingPrereq(OFType type, String name) {
 		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public boolean isCallbackOrderingPostreq(OFType type, String name) {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside isCallbackOrderingPostreq");
+		System.out.println("Inside isCallbackOrderingPostreq");
+		System.out.println("Inside isCallbackOrderingPostreq");*/
+		
 		return false;
 	}
 
 	@Override
 	public void switchAdded(long switchId) {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside switchAdded()");
+		System.out.println("Inside switchAdded()");
+		System.out.println("Inside switchAdded()");*/
 
 	}
 
 	@Override
 	public void switchRemoved(long switchId) {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside switchRemoved()");
+		System.out.println("Inside switchRemoved()");
+		System.out.println("Inside switchRemoved()");*/
 
 	}
 
 	@Override
 	public void switchActivated(long switchId) {
 		// TODO Auto-generated method stub
-
+		/*System.out.println("Inside switchActivated()");
+		System.out.println("Inside switchActivated()");
+		System.out.println("Inside switchActivated()");*/
+		
 	}
 
 	@Override
 	public void switchPortChanged(long switchId, ImmutablePort port,
 			PortChangeType type) {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside switchPortChanged()");
+		System.out.println("Inside switchPortChanged()");
+		System.out.println("Inside switchPortChanged()");*/
 
 	}
 
 	@Override
 	public void switchChanged(long switchId) {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside switchChanged()");
+		System.out.println("Inside switchChanged()");
+		System.out.println("Inside switchChanged()");*/
 
 	}
 
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside getModuleServices()");
+		System.out.println("Inside getModuleServices()");
+		System.out.println("Inside getModuleServices()");*/
+		
 		return null;
 	}
 
 	@Override
 	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside getServiceImpls()");
+		System.out.println("Inside getServiceImpls()");
+		System.out.println("Inside getServiceImpls()");*/
+		
 		return null;
 	}
 
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
 		// TODO Auto-generated method stub
+		/*
+		System.out.println("Inside getModuleDependencies()");
+		System.out.println("Inside getModuleDependencies()");
+		System.out.println("Inside getModuleDependencies()");*/
+		
 		return null;
 	}
 
@@ -167,6 +246,10 @@ public class Assignment3 implements ILinkDiscoveryListener, IOFMessageListener,
 	public void startUp(FloodlightModuleContext context)
 			throws FloodlightModuleException {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside startUp");
+		System.out.println("Inside startUp");
+		System.out.println("Inside startUp");*/
+		
 
 	}
 
@@ -174,19 +257,13 @@ public class Assignment3 implements ILinkDiscoveryListener, IOFMessageListener,
 	public net.floodlightcontroller.core.IListener.Command receive(
 			IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
 		// TODO Auto-generated method stub
+		/*System.out.println("Inside Ilistener.Command receive");
+		System.out.println("Inside Ilistener.Command receive");
+		System.out.println("Inside Ilistener.Command receive");*/
+		
+		
 		return null;
 	}
 
-	@Override
-	public void linkDiscoveryUpdate(LDUpdate update) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void linkDiscoveryUpdate(List<LDUpdate> updateList) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
